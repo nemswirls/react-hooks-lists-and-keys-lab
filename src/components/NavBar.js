728f49d8c3edc707847f4b178e1bf52a55f9be75
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-11-24 21:44:59 -0500

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => (
+        // Use the link name as the key for each <a> tag
+        <a href={`#${link}`} key={index}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
